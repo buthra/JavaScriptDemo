25aeaa507e4302ea1003a1704122ca2fed52a94d
user <user@DESKTOP-C2UJN49>
2018-03-16 00:38:03 +0100

    Tried calling JSON file using XMLHttpRequest function.

--- D1/Example/action.js
+++ D1/Example/action.js
@@ -1,4 +1,36 @@
-function myFunction(a, b) {
-    window.alert(return a * b));
-    
+
+//function load() {
+//	var  mydata = JSON.parse(data);
+//	for 
+//	alert(mydata[0].name);
+//	alert(mydata[0].age);
+//}
+
+
+function load()
+{
+  alert("Entering load");
+  var xmlhttp = new XMLHttpRequest();
+  
+  xmlhttp.overrideMimeType("application/json");
+
+  alert("before calling data.json");
+	xmlhttp.open("GET", 'data.json', true);
+	  alert("after calling data.json");
+	
+	xmlhttp.onreadystatechange = function() {
+	  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		   
+	        var myObj = JSON.parse(xmlhttp.responseText);
+	        alert("Inside IF");
+	        
+	        document.getElementById("demo").innerHTML = myObj.name;
+	  }
+	};
+	
+
+	xmlhttp.send();
+	 alert("Exiting load");
+	
 }
+
